8561327102f95693e12ac6d9fc9f052556292cfa
Jenny Qian <jqian@tendenci.com>
2015-04-08 10:08:47 -0500

    wrap @classmethod to get_model

--- case_studies/search_indexes.py
+++ case_studies/search_indexes.py
@@ -8,5 +8,6 @@ class CaseStudyIndex(TendenciBaseSearchIndex):
     text = indexes.CharField(document=True, use_template=True)
     client = indexes.CharField(model_attr='client')
 
+    @classmethod
     def get_model(self):
         return CaseStudy
